6d5441491d5ebf4c18b4a3011e781a61ea76a5f0
Debjoty Mitra <debjotyms@gmail.com>
2025-03-16 12:25:37 +0600

    Add OpenAI LLM invocation template

---
+++ Models/LLMs/01_openai_llms_invoke_template.py
@@ -0,0 +1,12 @@
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai_llm_model = OpenAI(model='gpt-3.5-turbo-instruct')
+result = openai_llm_model.invoke("What is meaning of AGI?")
+
+print(result)
+
+# load_dotenv() → Used to load API key from the .env file.
+# .invoke() → Takes a string as prompt and returns the result as a string.
